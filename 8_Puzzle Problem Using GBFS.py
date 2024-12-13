e1334f4702eaf1d85fa6e24a2a55f61c46ce55fd
Zainab Junaid <153438938+Zainab-Junaid@users.noreply.github.com>
2024-12-13 22:57:15 +0500

    Update 8_Puzzle Problem Using GBFS.py

--- 8_Puzzle Problem Using GBFS.py
+++ 8_Puzzle Problem Using GBFS.py
@@ -1,1 +1,12 @@
+import heapq
+
+class Node:
+    def __init__(self, state, parent=None, cost=0, heuristic=0):
+        self.state = state
+        self.parent = parent
+        self.cost = cost  # For GBFS, we don't use this, but it can be useful for comparison
+        self.heuristic = heuristic
+
+    def __lt__(self, other):
+        return self.heuristic < other.heuristic
 
